b1265581beecb89794938eb6e417c1b6071e8bf0
Mahdieh-tech <iammahdiehaghaei@gmail.com>
2025-11-08 15:36:59 +0330

    Card structure

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,23 @@
-export default function Card() {
+import React from "react";
+
+export default function Cards() {
+  const cardsData = [
+    { title: "خدمات تحویل داده شده", value: "44" },
+    { title: "خدمات درحال انجام", value: "34" },
+    { title: "مجموع خدمات", value: "78" },
+    { title: "فاکتورهای خرید", value: "82" },
+    { title: "فاکتورهای فروش", value: "100" },
+    { title: "مجموع فاکتورها", value: "182" },
+  ];
+
   return (
-    <p>hi</p>
+    <div className="">
+      {cardsData.map((card, index) => (
+        <div key={index} className="">
+          <h3 className="">{card.title}</h3>
+          <p className="">{card.value}</p>
+        </div>
+      ))}
+    </div>
   );
 }
