f8ceef885f8f1267cbe3a4e41cfba37c0ddc960d
Mahdieh-tech <iammahdiehaghaei@gmail.com>
2025-11-23 10:30:14 +0330

    Template.jsx responsived

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -9,15 +9,25 @@ export default function Template({
 }) {
   return (
     <div
-      className="grid grid-cols-12 gap-3 bg-gray-100/70 p-0 h-full pl-3"
+      className="
+        grid 
+        grid-cols-1
+        gap-3 
+        bg-gray-100/70 
+        p-0 
+        h-full 
+        pl-3
+
+        lg:grid-cols-12
+      "
     >
-      <div className="col-span-10 ">{header}</div>
-      <div className="col-span-2 row-span-4 ">{sidebar}</div>
-      <div className="col-span-3 ">{user}</div>
-      <div className="col-span-7 ">{chart}</div>
-      <div className="col-span-10 ">{cards}</div>
-      <div className="col-span-2 ">{stats}</div>
-      <div className="col-span-8 ">{checks}</div>
+      <div className="lg:col-span-10">{header}</div>
+      <div className="lg:col-span-2 lg:row-span-4">{sidebar}</div>
+      <div className="lg:col-span-3">{user}</div>
+      <div className="lg:col-span-7">{chart}</div>
+      <div className="lg:col-span-10">{cards}</div>
+      <div className="lg:col-span-2">{stats}</div>
+      <div className="lg:col-span-8">{checks}</div>
     </div>
   );
 }
